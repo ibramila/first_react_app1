4c64c8395a9026d15e264378759028921282b756
iammmila <ibramila2002@gmail.com>
2022-12-25 18:19:03 +0400

    toggle button

--- src/App.js
+++ src/App.js
@@ -1,46 +1,17 @@
-// import './App.css';
-// import Navbar from "./components/Navbar";
-// import Welcome from "./components/Welcome";
-// import Footer from "./components/Footer";
+import './App.css';
+import Box from './components/Box';
 
-// function App() {
-//   let users = [{
-//     id: 1,
-//     name: "malahat",
-//     age: 18,
-//     image: "https://media.gettyimages.com/id/1311737764/photo/flame-towers-in-baku-at-dusk.jpg?s=612x612&w=gi&k=20&c=_eY1WurnoRaLHfl38TNsrhwIF4m6siEq1ORPf7YfhrU=",
-//     hobbies: ["1","2"]
-//   }
-//     ,
-//   {
-//     id: 2,
-//     name: "malsdvsahat",
-//     age: 13,
-//     image: "https://media.gettyimages.com/id/1311737764/photo/flame-towers-in-baku-at-dusk.jpg?s=612x612&w=gi&k=20&c=_eY1WurnoRaLHfl38TNsrhwIF4m6siEq1ORPf7YfhrU=",
-//     hobbies: [1, 2, 5]
-//   }
-//   ]
-
-//   return (
-//     <>
-//       <Navbar />
-//       {users.map((user) => { return <Welcome key={user.id} name={user.name} age={user.age} image={user.image} hobbies={user.hobbies} /> })}
-//       <Footer />
-//     </>
-//   );
-// }
-
-
-// import Form from "./components/Form";
-import Box from "./components/Box";
+//!======================================== 
+//!        TOGGLE BUTTON
+//!======================================== 
 
 function App() {
-
   return (
     <>
-      {/* <Form /> */}
       <Box />
     </>
   );
 }
+
+
 export default App;
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react'; // ! TOOGLE BUTTON
 
-function Box({ label, toggled, onClick }) {
+function Box() {
     const [color1, setColor1] = useState(true);
     function changColor() {
         return setColor1(!color1);
     }
     return (
-
         <div>
             <button onClick={changColor}>change color</button>
             <div style={{ backgroundColor: color1 ? "red" : "green", width: 300, height: 300 }}>Toggle Button</div>
